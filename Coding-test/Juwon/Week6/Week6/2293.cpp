86d1f023948e318b67ec6e78b0e410763313da87
Juwon <juwon5196@gmail.com>
2024-03-18 23:05:54 +0900

    [주원] - 백준 2293번 동전 1 완료

---
+++ Coding-test/Juwon/Week6/Week6/2293.cpp
@@ -0,0 +1,43 @@
+﻿/*
+작성자: 이주원
+작성일자: 2024-03-18
+문제: 동전 1(https://www.acmicpc.net/problem/2293)
+
+가치합 문제
+경우를 나누자!
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int coins[101];
+int dp[100001];
+
+int main()
+{
+	int n;
+	int k;
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++)
+	{
+		// 동전 받아오기
+		cin >> coins[i];
+	}
+
+	dp[0] = 1;	
+	for (int i = 1; i <= n; i++)
+	{
+		// 동전 종류별로 넣어보기
+		for (int j = coins[i]; j <= k; j++)
+		{
+			dp[j] += dp[j - coins[i]];
+		}
+	}
+
+	cout << dp[k];
+
+	return 0;
+}
