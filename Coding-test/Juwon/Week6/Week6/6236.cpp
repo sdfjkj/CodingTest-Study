7acc76767057a435fb41778971194eb6fe1fa031
Juwon <juwon5196@gmail.com>
2024-03-24 21:41:51 +0900

    [주원] - 백준 29717번 슬라임 잡고 레벨 업 완료
    자료형 땜에 틀렸었다 ^&^

---
+++ Coding-test/Juwon/Week6/Week6/6236.cpp
@@ -0,0 +1,36 @@
+﻿/*
+작성자: 이주원
+작성일자: 2024-03-18
+문제: 용돈 관리(https://www.acmicpc.net/problem/6236)
+
+답 봄..ㅠ
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m, k, sum;
+vector<int> money;
+
+int main()
+{
+	cin >> n >> m;
+
+	sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		money.push_back(a);
+		sum += a;
+	}
+
+	k = sum / m; // k 초기값 설정
+
+
+
+
+	return 0;
+}
